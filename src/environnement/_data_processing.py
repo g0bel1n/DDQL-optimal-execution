c66593b030686d290c79db83b5d119745448871d
AuguMang <126862834+AuguMang@users.noreply.github.com>
2023-03-13 11:52:36 +0100

    inventory_action_transformer_v0
    Première version, erreurs potentielles à gérer et fonction inverse toujours pas tip top ça arrive bientôt

--- src/environnement/_data_processing.py
+++ src/environnement/_data_processing.py
@@ -30,4 +30,26 @@ def inventory_action_transformer(
     """
     # TODO: implement inventory_action_transformer according to Appendix A.1 
     # and add tests for it in tests/test_data_utils.py (if possible) (might need to implement a function to compute the inverse of the transformation)
+    batch_size, a = np.shape(inventory_action_pairs)
+    #if a != 2 :
+        #how do we handle errors ?
+    inventory_action_pairs_transformed = np.ndarray((batch_size,2))
+    for i in range(0,batch_size):
+        q,x=inventory_action_pairs[i,]
+        #if x > q :
+            #problem
+        hat_q = q/q0 - 1
+        hat_x = x/q0
+        r = np.sqrt(hat_q**2 + hat_x**2)
+        xi = -hat_x/hat_q
+        theta = np.arctan(xi)
+        if theta <= np.pi/4:
+            tilda_r = r *np.sqrt((xi**2+1)*(2*(np.cos(np.pi/4-theta))**2))
+        else :
+            tilda_r = r *np.sqrt((xi**(-2)+1)*(2*(np.cos(theta-np.pi/4))**2))
+        tilda_q = -tilda_r*np.cos(theta)
+        tilda_x = tilda_r*np.sin(theta)
+        inventory_action_pairs_transformed[i,] = tilda_q, tilda_x
+    return inventory_action_pairs_transformed
+    
     pass
