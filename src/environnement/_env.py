7b893e1e370c6929d9359fcca84c37c811180ea8
g0bel1n <lucas.saban@icloud.com>
2023-04-14 15:54:49 +0200

    multi episode

--- src/environnement/_env.py
+++ src/environnement/_env.py
@@ -1,8 +1,12 @@
-import pandas as pd
-from src import State
-import numpy as np
-from ._exceptions import InvalidActionError
+import os
 from typing import List
+
+import numpy as np
+import pandas as pd
+
+from src import State
+
+from ._exceptions import InvalidActionError, InvalidSwapError
 
 
 class MarketEnvironnement:
@@ -12,14 +16,31 @@ class MarketEnvironnement:
         data_path: str = "../data",
         intra_time_steps: int = 100,
         quadratic_penalty_coefficient: float = 0.01,
+        multi_episodes: bool = False,
     ) -> None:
         # multi episodes
         self.initial_inventory = initial_inventory
-        self.historical_data = pd.read_csv(f"{data_path}/historical_data.csv")
+        self.intra_time_steps = intra_time_steps
+
+        self.current_episode = 0
+        self.multi_episodes = multi_episodes
+
+        if multi_episodes:
+            self.historical_data_series = []
+            for file in os.listdir(data_path):
+                if file.endswith(".csv"):
+                    self.historical_data_series.append(f"{data_path}/{file}")
+
+            self.historical_data = pd.read_csv(
+                self.historical_data_series[self.current_episode]
+            )
+
+        else:
+            self.historical_data = pd.read_csv(f"{data_path}/historical_data.csv")
         self.historical_data = self.historical_data.set_index("Date")
         _date_splits = np.split(self.historical_data.index, intra_time_steps)
         self.historical_data["period"] = 0
-
+        self.historical_data["period"] = self.historical_data["period"].astype(int)
         for i, split in enumerate(_date_splits):
             self.historical_data.loc[split, "period"] = i
 
@@ -39,11 +60,38 @@ class MarketEnvironnement:
 
         self.quadratic_penalty_coefficient = quadratic_penalty_coefficient
 
+    def swap_episode(self, episode: int) -> None:
+        if self.state["period"] >=1 and not self.done:
+            raise InvalidSwapError
+
+        self.current_episode = episode
+        self.historical_data = pd.read_csv(self.historical_data_series[episode])
+        self.historical_data = self.historical_data.set_index("Date")
+        _date_splits = np.split(self.historical_data.index, self.intra_time_steps)
+        self.historical_data["period"] = 0
+
+        for i, split in enumerate(_date_splits):
+            self.historical_data.loc[split, "period"] = i
+
+        # agent move along Ts while reward along ts
+        self.horizon = len(_date_splits)
+
+        self.done = False
+
+        self.state = State(
+            dict(
+                zip(
+                    [*self.historical_data.columns, "inventory", "reward"],
+                    [*self.historical_data.iloc[0].values, self.initial_inventory, 0],
+                )
+            )
+        )
+
     def step(self, action: int) -> tuple:
         # Execute one time step within the environment
 
         if action > self.state["inventory"]:
-            raise InvalidActionError("Sell order is greater than inventory")
+            raise InvalidActionError
 
         self._execute_action(action)
 
@@ -98,4 +146,4 @@ class MarketEnvironnement:
         self.done = False
 
     def __repr__(self) -> str:
-        return f"MarketEnvironnement(initial_inventory={self.initial_inventory}, quadratic_penalty_coefficient={self.quadratic_penalty_coefficient})"
+        return f"MarketEnvironnement(initial_inventory={self.initial_inventory}, quadratic_penalty_coefficient={self.quadratic_penalty_coefficient}, current_episode={self.current_episode}, multi_episodes={self.multi_episodes})"
--- src/environnement/_exceptions.py
+++ src/environnement/_exceptions.py
@@ -5,7 +5,21 @@ class InvalidActionError(Exception):
         message -- explanation of the error
     """
 
-    def __init__(self, message):
+    def __init__(self, message = "Sell order is greater than inventory"):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+    
+
+class InvalidSwapError(Exception):
+    """Exception raised when an episode is swapped while in the middle of an episode.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message = "Cannot swap episode while in the middle of an episode"):
         self.message = message
 
     def __str__(self):
